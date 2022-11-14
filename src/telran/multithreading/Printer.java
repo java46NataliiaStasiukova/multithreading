ab8275469cb17a90adf72983c826569f372fd4d1
Nataliia <nstasiukova@gmail.com>
2022-11-14 21:14:56 +0200

    Classwork-40

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -1,28 +1,36 @@
 package telran.multithreading;
 
-
 public class Printer extends Thread {
-	private int nPrints;
-	private char symb;
-	
-	public Printer(int nPrints, char symb) {
-		this.nPrints = nPrints;
-		this.symb = symb;
+	private static final long TIMEOUT = 2000;
+	private String symbols;
+	private boolean running = true;
+	public boolean isRunning() {
+		return running;
 	}
-	
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+	public Printer(String symbols) {
+		this.symbols = symbols;
+		//setDaemon(true);
+	}
 	@Override
 	public void run() {
-		for(int i = 0; i < nPrints; i++) {
+		int length = symbols.length();
+		int index = 0;
+		while(running) {
+			System.out.println(symbols.charAt(index));
 			try {
-				sleep(10);//приостанавливается
+				sleep(TIMEOUT);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				index++;
+				if(index == length) {
+					index = 0;
+				}
 			}
-			System.out.println(symb);
 		}
-	
 	}
-
 	
-
+	
+	
 }
