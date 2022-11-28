f177c6ccfe828265c21d0456e60a479ecf276bd3
Nataliia <nstasiukova@gmail.com>
2022-11-28 20:38:18 +0200

    Classwork-44 MessageBox with notifyAll and Daemon receiver

--- src/telran/multithreading/MessageBox.java
+++ src/telran/multithreading/MessageBox.java
@@ -3,10 +3,13 @@ package telran.multithreading;
 public class MessageBox {
 private String message;
 
-public void put(String message) {
+public void put(String message) throws InterruptedException {
 	synchronized(this) {
+		while(this.message != null) {
+			this.wait();
+		}
 		this.message = message;
-		this.notify();
+		this.notifyAll();
 	}
 }
 public String get() throws InterruptedException {
@@ -16,6 +19,7 @@ public String get() throws InterruptedException {
 		}
 		String res = message;
 		message = null;
+		this.notifyAll();
 		return res;
 	}
 }
